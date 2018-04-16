9a8bc06b062d11ba11e83f948042f9d666e1bebc
miriam30 <miriam.alvarez.p96@gmail.com>
2018-04-15 22:54:27 -0500

    able to check if number is prime or not

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*COMPONENTS*/
+import PrimeNum from './component/numbers/NumList.jsx'
 const App = () => {
   return (
     <div>
       <h1 > Hello world! </h1>
+      <PrimeNum />
     </div>
 )
 }
--- client/src/component/numbers/NumList.jsx
+++ client/src/component/numbers/NumList.jsx
@@ -4,54 +4,47 @@ import $ from 'jquery';
 class PrimeNum extends React.Component {
   constructor(props){
     super(props)
+
     this.state={
-      numbers=[],
-      num=0
+      numbers: [],
+      number: 0
     }
-    this.getNumbers=this.getNumbers.bind(this);
+
   }
   /*ajax to api*/
-    getNumbers(){
-      $.ajax({
-        url:"http://174.138.36.217/numbers/",
-        method: 'GET',
-        success: (nums) => {
-          this.setState({
-            numbers: nums
-          })
-        },
-        error: ( err) => {
-        console.log('not geting numbers', err);
-      }
-      })
-    }
-    /* function to select if prime or not*/
-    var primeTester = function(num) {
-  if (typeof num !== 'number' || num < 1 || num % 1 !== 0) {
-    return false;
+  getNumbers(){
+    $.ajax({
+     url: 'http://174.138.36.217/numbers/',
+     method: 'GET',
+     success: (data) => {
+       this.setState({
+         numbers: data});
+         console.log(data);
+     },
+     error: ( xhr, err ) => {
+       console.log('err', err);
+     }
+    })
   }
-  if(num === 2){num
-    return num+" Is Prime"
-  }
-  for(var i =2;i<num;i++){
-    if(num%i !== 0){
-      return num+" Is Prime"
-    } else {
-      return num+" Totally not prime"
-    }
-  }
+/* function to select if prime or not*/
+primeTester(n) {
+  for(var i =2; i<n;i++)
+  if(n % i === 0) return n + " Totally not prime";
+  return n + " Is Prime";
 }
 
 componentDidMount(){
   this.getNumbers()
 }
 
-render {
+render() {
   return(
     <div>
-       <ul>
-         {this.state.numbers.map(nums => <li>{this.getNumbers(nums.number)}</li>)}
-       </ul></div>
-  )
+    <ul>
+    { this.state.numbers.map(digitos => <li>{this.primeTester(digitos.number)}</li>)}
+    </ul>
+    </div>
+    )
+  }
 }
-}
+export default PrimeNum;
